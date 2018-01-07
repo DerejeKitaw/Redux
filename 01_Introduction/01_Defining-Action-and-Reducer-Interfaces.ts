94c9ad13eea7c21fb383cc66cd27fc5b7d77ed79
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-07 14:15:36 -0500

    Bydefault,reducers return the original state

---
+++ 01_Introduction/01_Defining-Action-and-Reducer-Interfaces.ts
@@ -0,0 +1,19 @@
+interface Action {
+    type: string;
+    payload?: any;
+  }
+  
+  interface Reducer<T> {
+    (state: T, action: Action): T;
+  }
+  
+  let reducer: Reducer<number> = (state: number, action: Action) => {
+    return state;
+  };
+  
+  // Bydefault,reducers return the original state. 
+  // In this case, we passed a state of the number 0 
+  // and a null action. The result from this reducer 
+  // is the state 0. 
+  console.log( reducer(0, null) ); // -> 0
+  
