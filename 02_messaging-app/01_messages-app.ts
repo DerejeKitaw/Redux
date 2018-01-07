f8175c23c1bcd72289f2ef9218832d427f07bd70
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-07 17:52:03 -0500

    define reducer

--- 02_messaging-app/01_messages-app.ts
+++ 02_messaging-app/01_messages-app.ts
@@ -22,7 +22,27 @@ interface AddMessageAction extends Action {
 //#endregion
 
 //#region 03_Define reducer
-
+let reducer: Reducer<AppState> =
+  (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+  case 'ADD_MESSAGE':
+    return {
+      messages: state.messages.concat(
+        (<AddMessageAction>action).message
+      ),
+    };
+  case 'DELETE_MESSAGE':
+    let idx = (<DeleteMessageAction>action).index;
+    return {
+      messages: [
+        ...state.messages.slice(0, idx),
+        ...state.messages.slice(idx + 1, state.messages.length)
+      ]
+    };
+  default:
+    return state;
+  }
+};
 //#endregion 
 
 //#region 04_create new store
