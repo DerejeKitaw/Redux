79e33de06e9613bb42a21d0e8bc0f842571dcb91
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-25 23:56:27 -0500

    format

--- 03_using-real-redux/01_messages-app-using-redux.ts
+++ 03_using-real-redux/01_messages-app-using-redux.ts
@@ -1,9 +1,4 @@
-import {
-  Action,
-  Reducer,
-  Store,
-  createStore
-} from 'redux';
+import { Action, Reducer, Store, createStore } from 'redux';
 
 //#region 01_Define interfaces
 interface AppState {
@@ -17,7 +12,7 @@ interface AddMessageAction extends Action {
 interface DeleteMessageAction extends Action {
   index: number;
 }
-//#endregion 
+//#endregion
 
 //#region 01a_Define action creator
 class MessageActions {
@@ -34,33 +29,35 @@ class MessageActions {
     };
   }
 }
-//#endregion 
+//#endregion
 
 //#region 02_Define-Store
 // Done by Redux
 //#endregion
 let initialState: AppState = { messages: [] };
 //#region 03_Define reducer
-let reducer: Reducer<AppState> =
-  (state: AppState = initialState, action: Action) => {
+let reducer: Reducer<AppState> = (
+  state: AppState = initialState,
+  action: Action
+) => {
   switch (action.type) {
-  case 'ADD_MESSAGE':
-    return {
-      messages: state.messages.concat((<AddMessageAction>action).message),
-    };
-  case 'DELETE_MESSAGE':
-    let idx = (<DeleteMessageAction>action).index;
-    return {
-      messages: [
-        ...state.messages.slice(0, idx),
-        ...state.messages.slice(idx + 1, state.messages.length)
-      ]
-    };
-  default:
-    return state;
+    case 'ADD_MESSAGE':
+      return {
+        messages: state.messages.concat((<AddMessageAction>action).message)
+      };
+    case 'DELETE_MESSAGE':
+      let idx = (<DeleteMessageAction>action).index;
+      return {
+        messages: [
+          ...state.messages.slice(0, idx),
+          ...state.messages.slice(idx + 1, state.messages.length)
+        ]
+      };
+    default:
+      return state;
   }
 };
-//#endregion 
+//#endregion
 
 //#region 04_create new store
 let store: Store<AppState> = createStore<AppState>(reducer);
@@ -68,35 +65,38 @@ console.log(store.getState()); // -> { messages: [] }
 //#endregion
 
 //#region 05_subscribe to store
-  // Done by Redux
+// Done by Redux
 //#endregion
 
 //#region 06_Dispatch store
 store.dispatch(
-  MessageActions.addMessage('Would you say the fringe was made of silk?'));
+  MessageActions.addMessage('Would you say the fringe was made of silk?')
+);
 
 store.dispatch(
-  MessageActions.addMessage('Wouldnt have no other kind but silk'));
+  MessageActions.addMessage('Wouldnt have no other kind but silk')
+);
 
 store.dispatch(
-  MessageActions.addMessage('Has it really got a team of snow white horses?'));
+  MessageActions.addMessage('Has it really got a team of snow white horses?')
+);
 
 console.log(store.getState());
-// -> 
+// ->
 // { messages:
 //    [ 'Would you say the fringe was made of silk?',
 //      'Wouldnt have no other kind but silk',
 //      'Has it really got a team of snow white horses?' ] }
 
-store.dispatch( MessageActions.deleteMessage(1) );
+store.dispatch(MessageActions.deleteMessage(1));
 
 console.log(store.getState());
-// -> 
+// ->
 // { messages:
 //    [ 'Would you say the fringe was made of silk?',
 //      'Has it really got a team of snow white horses?' ] }
 
-store.dispatch( MessageActions.deleteMessage(0) );
+store.dispatch(MessageActions.deleteMessage(0));
 
 console.log(store.getState());
 // ->
