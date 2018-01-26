a0252873cebcdd2c54a4187c2577d7c094971b19
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-01-25 23:57:42 -0500

    format

--- 01_Introduction/02_Defining-Action-and-Reducer-Interfaces.ts
+++ 01_Introduction/02_Defining-Action-and-Reducer-Interfaces.ts
@@ -1,36 +1,32 @@
 interface Action {
-    type: string;
-    payload?: any;
+  type: string;
+  payload?: any;
+}
+interface Reducer<T> {
+  (state: T, action: Action): T;
+}
+
+// Define Actions
+let incrementAction: Action = { type: 'INCREMENT' };
+let decrementAction: Action = { type: 'DECREMENT' };
+
+let reducer: Reducer<number> = (state: number, action: Action) => {
+  if (action.type === 'INCREMENT') {
+    return state + 1;
   }
-  interface Reducer<T> {
-    (state: T, action: Action): T;
+  if (action.type === 'DECREMENT') {
+    return state - 1;
   }
+  // If there is no action defined return state
+  // State is not mutable
+  return state;
+};
 
-  // Define Actions
-  let incrementAction: Action = { type: 'INCREMENT' };
-  let decrementAction: Action = { type: 'DECREMENT' };
+// In this case, we passed a state of the number 0
+// and a INCREMENT action. The result from this reducer
+// is the state 1.
 
-  
-  let reducer: Reducer<number> = (state: number, action: Action) => {
-    if (action.type === 'INCREMENT') {
-      return state + 1;
-    }
-    if (action.type === 'DECREMENT') {
-      return state - 1;
-    }
-    // If there is no action defined return state
-    // State is not mutable
-    return state;
-  };
-  
-  // In this case, we passed a state of the number 0
-  // and a INCREMENT action. The result from this reducer 
-  // is the state 1. 
+console.log(reducer(0, incrementAction)); // -> 1
+console.log(reducer(1, incrementAction)); // -> 2
 
-  console.log( reducer(0, incrementAction )); // -> 1
-  console.log( reducer(1, incrementAction )); // -> 2
-  
-  
-  console.log( reducer(100, decrementAction )); // -> 99
-  
-  
+console.log(reducer(100, decrementAction)); // -> 99
