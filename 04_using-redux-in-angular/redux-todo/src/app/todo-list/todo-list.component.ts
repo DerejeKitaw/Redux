5f6d866d93759cb356c755aa02bff9c494a132df
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-03-04 14:39:49 -0500

    import App state , actions and stor

--- 04_using-redux-in-angular/redux-todo/src/app/todo-list/todo-list.component.ts
+++ 04_using-redux-in-angular/redux-todo/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgRedux, select } from '@angular-redux/store';
+import { IAppState} from '../store';
+import {ADD_TODO, REMOVE_TODO, REMOVE_ALL_TODOS } from '../actions';
+
 
 @Component({
   selector: 'app-todo-list',
